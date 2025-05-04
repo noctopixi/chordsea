059ebe08a1629b572859fa97a5941fc8ee6adf87
noctopixi <164736486+noctopixi@users.noreply.github.com>
2025-05-03 23:12:44 -0600

    feat(flask): render the output of generate_chords() in the webapp
    routes.py passes the output of generate_chords as homepage_random_chords
    to index.html as a variable named chords.
    
    index.html displays chords and their tabs in a monospaced code block.

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,12 @@
 from app import app
+from app.generator import generate_chords
 from flask import render_template
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    homepage_random_chords = generate_chords(no_tab=True)
+    return render_template("index.html", chords=homepage_random_chords)
 
 
 @app.route("/about")
