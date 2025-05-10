ecb9b85f77f793ff37862d76b9fa7a48384ade03
noctopixi <164736486+noctopixi@users.noreply.github.com>
2025-05-09 22:06:56 -0600

    feat(flask): add support for customizing chord generation
    Added a form for customizing how many chord sets are generated, as well
    as how many chords are included in each set.
    
    routes.py now detects if the POST method is being used and captures the
    values submitted in the form, then passes them to generate_chords().
    
    This change achieves feature parity with the CLI version of the program.

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,20 @@
 from app import app
 from app.generator import generate_chords
 from flask import render_template
+from flask import request # Proxy representing the form
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    homepage_random_chords = generate_chords(no_tab=True)
+    if request.method == "POST":
+        # Get the values of the form by the 'name' attribute and convert to int
+        count = request.form.get("count", type=int)
+        sets = request.form.get("sets", type=int)
+        homepage_random_chords = generate_chords(count=count, sets=sets, no_tab=True)
+    else:
+        # Generate tabs with default values if customization form was not sent
+        homepage_random_chords = generate_chords(no_tab=True)
+
     return render_template("index.html", chords=homepage_random_chords)
 
 
